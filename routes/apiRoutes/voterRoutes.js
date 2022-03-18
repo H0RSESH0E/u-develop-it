174550a93f00e20d5244396227e892f03816c9b4
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-03-18 13:17:15 -0400

    Party Routes Modularized

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection.js');
+const inputCheck = require('../../utils/inputCheck.js');
+
+
+module.exports = router;
